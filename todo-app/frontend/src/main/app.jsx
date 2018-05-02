8add61a70db3bbc8bafaeb3fa0c94c412e349d58
G. Silva De Menezes <g.silva.de.menezes@avanade.com>
2018-05-02 18:35:53 -0300

    Add a few thing on the main page

--- todo-app/frontend/src/main/app.jsx
+++ todo-app/frontend/src/main/app.jsx
@@ -2,10 +2,12 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
-import Todo from '../todo/todo'
+import Routes from './routes'
+import Menu from '../template/menu'
 
 export default props=> (
     <div className='container'>
-        <Todo />
+        <Menu />
+        <Routes />
     </div>
 )
--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
+import PageHeader from '../template/pageHeader'
+import TodoForm from './todoForm'
+import TodoList from './todoList'
 
 export default class Todo extends Component{
+    constructor(props){
+        super(props)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleAdd(){
+        console.log(this)
+    }
+
     render(){
         return(
             <div>
-                <h1>Todo</h1>
+                <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
+                <TodoForm handleAdd={this.handleAdd}/>
+                <TodoList />
             </div>
         )
     }
